72fbd5d4b94f1229a15b4c68e26817e340d119df
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-01-12 08:37:46 +0700

    Initialize migrations.

---
+++ migrations/create_roles_table.php
@@ -0,0 +1,20 @@
+<?php
+
+class create_roles_table
+{
+    /**
+     * @return void
+     */
+    public function up(PDO $connection)
+    {
+        //
+    }
+
+    /**
+     * @return void
+     */
+    public function down(PDO $connection)
+    {
+        //
+    }
+}
---
+++ migrations/create_users_table.php
@@ -0,0 +1,20 @@
+<?php
+
+class create_users_table
+{
+    /**
+     * @return void
+     */
+    public function up(PDO $connection)
+    {
+        //
+    }
+
+    /**
+     * @return void
+     */
+    public function down(PDO $connection)
+    {
+        //
+    }
+}
